bb2af58b300e892728a319f741791d66bbf3641e
Zero825 <k1114234030@live.com>
2020-07-29 14:08:44 +0800

    修复bug

--- app/src/main/java/com/news/gamersky/MainActivity.java
+++ app/src/main/java/com/news/gamersky/MainActivity.java
@@ -1,45 +1,31 @@
 package com.news.gamersky;
 
+import android.animation.ObjectAnimator;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.news.gamersky.fragment.NewsFragment;
+import com.news.gamersky.util.AppUtil;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.github.piasy.biv.BigImageViewer;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-import com.news.gamersky.fragment.EntertainmentFragment;
-import com.news.gamersky.fragment.HomePageFragment;
-import com.news.gamersky.fragment.InterestingImagesFragment;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageView logo;
-    private ImageView searchBtn;
-    private ImageView setBtn;
-    private ViewPager2 viewPager2;
-    private TabLayout tabLayout;
-    private FragmentAdapter fragmentAdapter;
-    private SharedPreferences sharedPreferences;
     private long exitTime;
+    private BottomNavigationView navView;
+    private ObjectAnimator showAnimator;
+    private FragmentManager fragmentManager;
+    private FrameLayout hostContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,120 +33,73 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         startListen();
-
     }
 
-    private void init(){
+    public void init(){
         exitTime=0;
 
         getWindow().getDecorView()
-                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        logo=findViewById(R.id.imageView4);
-        searchBtn=findViewById(R.id.imageView11);
-        setBtn=findViewById(R.id.imageView10);
-        viewPager2=findViewById(R.id.viewPager);
-        tabLayout=findViewById(R.id.tabLayout);
-        fragmentAdapter=new FragmentAdapter(this);
-        viewPager2.setAdapter(fragmentAdapter);
-        viewPager2.setOffscreenPageLimit(fragmentAdapter.getItemCount());
-        new TabLayoutMediator(tabLayout, viewPager2,
-                new TabLayoutMediator.TabConfigurationStrategy() {
-                    @Override
-                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                        if(position==0) tab.setText("首页");
-                        if(position==1)tab.setText("娱乐");
-                        if(position==2) tab.setText("囧图");
-                    }
-                }
-        ).attach();
-        sharedPreferences =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        clearGlideDiskCache(sharedPreferences.getBoolean("auto_clear_cache",true));
-    }
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+        navView = findViewById(R.id.nav_view);
+        hostContainer=findViewById(R.id.nav_host_container);
 
-    private void startListen(){
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SearchActivity.class));
-            }
-        });
-        setBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SettingsActivity.class));
-            }
-        });
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int p=viewPager2.getCurrentItem();
-                if(fragmentAdapter.getFragment(p)!=null){
-                    Fragment fragment=fragmentAdapter.getFragment(p);
-                    if(p==0) {
-                        ((HomePageFragment) fragment).upTop();
-                    }
-                    if(p==1) {
-                        ((EntertainmentFragment) fragment).upTop();
-                    }
-                    if(p==2) {
-                        ((InterestingImagesFragment) fragment).upTop();
-                    }
-                }
-            }
-        });
+        fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.nav_host_container, new NewsFragment(), "newsFragment");
+        fragmentTransaction.commit();
 
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                for (int i=0;i<fragmentAdapter.getItemCount();i++){
-                   endAnimator(tabLayout.getTabAt(i).view);
-                }
-                startAnimator(tabLayout.getTabAt(position).view);
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
-            }
-        });
-    }
-
-    public void clearGlideDiskCache(boolean b){
-        if(b) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Glide.get(MainActivity.this).clearDiskCache();
-                    BigImageViewer.imageLoader().cancelAll();
-                }
-            }).start();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if(sharedPreferences.getBoolean("no_bottombar",true)){
+            navView.setVisibility(View.GONE);
         }
     }
 
-    public void startAnimator(View view){
-        ObjectAnimator objectAnimator1 = ObjectAnimator.ofFloat(view, "ScaleX", view.getScaleX(),1.15f);
-        ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(view, "ScaleY", view.getScaleY(), 1.15f);
-        AnimatorSet animSet = new AnimatorSet();
-        animSet.play(objectAnimator1).with(objectAnimator2);
-        animSet.setDuration(300);
-        animSet.start();
+    public void startListen(){
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                hideAllFragment(fragmentTransaction);
+                Log.i("TAG", "onNavigationItemSelected: show"+item.getItemId());
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        if(fragmentManager.findFragmentByTag("newsFragment")!=null){
+                            fragmentTransaction.show(fragmentManager.findFragmentByTag("newsFragment"));
+                        }else {
+                            fragmentTransaction.add(R.id.nav_host_container, new NewsFragment(), "newsFragment");
+                        }
+
+                        break;
+                }
+                Log.i("TAG", "onNavigationItemSelected: "+item+item.getItemId());
+                fragmentTransaction.commit();
+                return true;
+            }
+
+
+        });
+
+        navView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        if(fragmentManager.findFragmentByTag("newsFragment")!=null){
+                            Fragment fragment=fragmentManager.findFragmentByTag("newsFragment");
+                            ((NewsFragment) fragment).upTop();
+                        }
+                        break;
+                }
+            }
+        });
+
     }
 
-    public void endAnimator(View view){
-        ObjectAnimator objectAnimator1 = ObjectAnimator.ofFloat(view, "ScaleX", view.getScaleX(), 1.0f);
-        ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(view, "ScaleY", view.getScaleY(), 1.0f);
-        AnimatorSet animSet = new AnimatorSet();
-        animSet.play(objectAnimator1).with(objectAnimator2);
-        animSet.setDuration(300);
-        animSet.start();
+    private void hideAllFragment(FragmentTransaction fragmentTransaction){
+        if(fragmentManager.findFragmentByTag("newsFragment")!=null){
+            fragmentTransaction.hide(fragmentManager.findFragmentByTag("newsFragment"));
+        }
     }
 
     @Override
@@ -170,51 +109,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void exitApp() {
         if ((System.currentTimeMillis() - exitTime) > 2000) {
-            Snackbar.make(viewPager2,"再按一次退出应用",1000).show();
+            AppUtil.getSnackbar(this,hostContainer,"首页刷新成功",false,true).show();
             exitTime = System.currentTimeMillis();
         } else {
             finish();
         }
     }
 
-    public class FragmentAdapter extends FragmentStateAdapter {
-        public HashMap<Integer,Fragment> fragmentHashMap;
-
-        public FragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
-            fragmentHashMap=new HashMap<>();
-        }
-
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            Fragment fragment=new Fragment();
-            if(position==0){
-                fragment= new HomePageFragment();
-            }
-            if(position==1){
-                fragment= new EntertainmentFragment();
-            }
-            if(position==2){
-                fragment= new InterestingImagesFragment();
-            }
-            fragmentHashMap.put(position,fragment);
-            return fragment;
-        }
-
-
-
-        @Override
-        public int getItemCount() {
-            return 3;
-        }
-
-        public Fragment getFragment(int position){
-            if(fragmentHashMap.containsKey(position)){
-                return fragmentHashMap.get(position);
-            }else {
-                return null;
-            }
+    public void showNav(){
+        if(showAnimator==null||!showAnimator.isRunning()) {
+            showAnimator = ObjectAnimator.ofFloat(navView, "translationY", navView.getTranslationY(), 0f);
+            showAnimator.setDuration(300);
+            showAnimator.start();
         }
     }
+
 }
