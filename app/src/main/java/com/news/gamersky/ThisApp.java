4396fd5f136c9f0a5d51d837d6ffdfdb5085adcc
Zero825 <k1114234030@live.com>
2020-11-07 20:13:06 +0800

    添加加载开关

--- app/src/main/java/com/news/gamersky/ThisApp.java
+++ app/src/main/java/com/news/gamersky/ThisApp.java
@@ -2,15 +2,9 @@ package com.news.gamersky;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
-
-import androidx.preference.PreferenceManager;
 
 import com.github.piasy.biv.BigImageViewer;
 import com.github.piasy.biv.loader.glide.GlideImageLoader;
-import com.news.gamersky.setting.AppSetting;
-import com.news.gamersky.util.NightModeUtil;
 
 
 public class ThisApp extends Application{
